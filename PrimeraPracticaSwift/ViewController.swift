1425fa9056a32b13c013e48601f9dd31b08567b3
AgustinCarbajal1997 <magustincarbajal97@gmail.com>
2023-10-21 20:51:46 -0300

    [feature] picker view

--- PrimeraPracticaSwift/ViewController.swift
+++ PrimeraPracticaSwift/ViewController.swift
@@ -13,6 +13,12 @@ class ViewController: UIViewController {
     //Outlets
     @IBOutlet weak var myButton: UIButton!
 
+    @IBOutlet weak var myPicker: UIPickerView!
+    
+    
+    //variables
+    private let myPickerViewValues = ["Uno", "Dos", "tres", "cuatro", "cinco"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -20,6 +26,14 @@ class ViewController: UIViewController {
         myButton.setTitle("Mi Botón", for: .normal)
         myButton.backgroundColor = .blue
         myButton.setTitleColor(.white, for: .normal)
+        
+        //pickers
+        myPicker.backgroundColor = .lightGray
+        // para poder hacer el data source y el delegate vamos a necesitar extender con "extension", para separa nuetro codigo
+        myPicker.dataSource = self //le estamos indicando que clase se va a encargar de proporcionar los elementos y le decimos que la propia, osea el propio viewcontroller. Implementamos su protocolo, el cual nos va a ayudar a cargarle datos
+        myPicker.delegate = self // sobre quien queremos crear el delegado, sobre el view controller
+        
+        
     }
 
     //actions
@@ -33,3 +47,35 @@ class ViewController: UIViewController {
     }
 }
 
+//son los dos protocolos que queremos delegar sobre ViewController
+extension ViewController: UIPickerViewDataSource, UIPickerViewDelegate{
+    
+    //datasource habitualmente sirve para cargar datos en la vista
+    //delegate sirve para interactuar con el viewcontroller
+    
+    // Aca empiezan las de data source
+    // numero de columnas
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    // numero de items
+    //COMPONENTE SIRVE PARA VER EN CUAL PICKER VIEW ESTAMOS PORQUE PODEMOS TENER VARIOS
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        //seria el equivalente al .length
+        return myPickerViewValues.count
+    }
+    
+    //los elementos que se van a mostrar
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return myPickerViewValues[row]
+    }
+    
+    
+    //aca empiezan las de delegate
+    //vamos a seleccionar un elemento
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let myString = myPickerViewValues[row]
+        myButton.setTitle(myString, for: .normal)
+    }
+}
